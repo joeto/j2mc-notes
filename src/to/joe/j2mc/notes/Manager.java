5cbf9d5188b67f245d2de7944782111a48b5a829
Ammar Askar <ammar_askar@hotmail.com>
2012-04-21 21:28:28 +0500

    First nyan

---
+++ src/to/joe/j2mc/notes/Manager.java
@@ -0,0 +1,59 @@
+package to.joe.j2mc.notes;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+
+import to.joe.j2mc.notes.util.Note;
+
+import to.joe.j2mc.core.J2MC_Manager;
+
+public class Manager {
+
+    J2MC_Notes plugin;
+
+    public Manager(J2MC_Notes notes) {
+        this.plugin = notes;
+    }
+
+    public void AddNote(String sender, String target, String message, boolean admin) {
+        try {
+            PreparedStatement ps = J2MC_Manager.getMySQL().getFreshPreparedStatementHotFromTheOven("INSERT INTO notes (`from`,`to`,`message`,`time`,`adminBusiness`) VALUES (?,?,?,?,?)");
+            ps.setString(1, sender);
+            ps.setString(2, target);
+            ps.setString(3, message);
+            ps.setTimestamp(4, new Timestamp(new Date().getTime()));
+            ps.setBoolean(5, admin);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            plugin.getLogger().warning("Error recording a note: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+
+        }
+    }
+
+    public ArrayList<Note> grabNotes(String target) {
+        final ArrayList<Note> notes = new ArrayList<Note>();
+        PreparedStatement ps;
+        try {
+            ps = J2MC_Manager.getMySQL().getFreshPreparedStatementHotFromTheOven("SELECT * FROM notes where received=0 and `to`=?");
+            ps.setString(1, target);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                notes.add(new Note(rs.getString("from"), rs.getString("message"), new Date(rs.getTimestamp("time").getTime()), rs.getBoolean("adminBusiness")));
+            }
+            PreparedStatement prep = J2MC_Manager.getMySQL().getFreshPreparedStatementHotFromTheOven("UPDATE notes SET received=1 where `to`=?");
+            prep.setString(1, target);
+            prep.executeUpdate();
+        } catch (SQLException e) {
+            plugin.getLogger().warning("Unable to load user notes from MySQL: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            
+        }
+        return notes;
+    }
+
+}
---
+++ src/to/joe/j2mc/notes/util/Note.java
@@ -0,0 +1,34 @@
+package to.joe.j2mc.notes.util;
+
+import java.util.Date;
+
+import org.bukkit.ChatColor;
+
+/**
+ * User to user notes
+ * 
+ */
+public class Note {
+    private final String sender;
+    private final String message;
+    private final Date time;
+    boolean adminBusiness;
+
+    public Note(String sender, String message, Date time, boolean adminBusiness) {
+        this.sender = sender;
+        this.message = message;
+        this.time = time;
+        this.adminBusiness = adminBusiness;
+    }
+
+    @Override
+    public String toString() {
+        String from;
+        if (this.adminBusiness) {
+            from = "ADMIN";
+        } else {
+            from = this.sender;
+        }
+        return ChatColor.AQUA + "[" + this.time + "] <" + from + "> " + ChatColor.DARK_AQUA + this.message;
+    }
+}
